c72208a66e24ea165aa75b1f3b3d133afc732a4d
Manasvini Sethuraman <msethuraman3@gatech.edu>
2021-04-08 16:02:48 -0400

    initial commit

---
+++ src/redis-messages.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDED_REDISMESSAGES_H
+#define INCLUDED_REDISMESSAGES_H
+
+class RedisMessages{
+};
+#endif
---
+++ test/test.cc
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "redis-messages.h"
+
+using ::testing::Test;
+
+class MetadataTester : public ::testing::Test{
+
+};
+
+TEST_F(MetadataTester, DummyTest){
+}
