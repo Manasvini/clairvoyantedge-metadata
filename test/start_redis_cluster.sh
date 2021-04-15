1b403c2549da261acf6d60a6c42ea4a28740b8f7
Manasvini <msethuraman3@gatech.edu>
2021-04-15 20:47:29 +0000

    add message definitions

--- test/start_redis_cluster.sh
+++ test/start_redis_cluster.sh
@@ -10,7 +10,8 @@ workdir=$PWD
 echo $workdir
 echo "starting redis cluster with total $NUM_NODES_TOTAL nodes"
 
-address="127.0.0.1:"
+address="192.168.123.50"
+charcolon=":"
 addresses=""
 for ((i=0;i<NUM_NODES_TOTAL;i++));
 do
@@ -19,17 +20,18 @@ do
     port_num=$((i+start_port))
     echo "starting node on $port_num"
     echo "port $port_num" > redis.conf
+    echo "bind  0.0.0.0" >> redis.conf
     echo "cluster-enabled yes" >> redis.conf
     echo "cluster-config-file node.conf" >> redis.conf
     echo "cluster-node-timeout 5000" >> redis.conf
     echo "appendonly yes" >> redis.conf
-    redis-server redis.conf --daemonize yes
+    redis-server redis.conf --daemonize yes 
     cd $workdir
-    addresses="$addresses $address$port_num "
+    addresses="$addresses $address$charcolon$port_num "
 done
 
 echo "addresses: $addresses"
 
-redis-cli --cluster create $addresses --cluster-replicas 1
+redis-cli --cluster create $addresses --cluster-replicas 1 
  
     
